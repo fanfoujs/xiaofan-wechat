dd7716f545135b5092ac7efa3d50847b66ea3a5a
Catt Liu <maundytime@gmail.com>
2017-09-21 02:22:51 +0800

    add query save function

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,8 +1,9 @@
 const tab = require('../../components/tab')
 const fm = require('../../components/feeds-manager')
 const ff = require('../../utils/fanfou')
+
 const url = '/statuses/public_timeline'
-const {TIMELINE_COUNT} = require('../../config/fanfou')
+const { TIMELINE_COUNT } = require('../../config/fanfou')
 const para = { count: TIMELINE_COUNT }
 
 Page({
@@ -17,7 +18,7 @@ Page({
     fm.load(this, url, para)
     this.loadTrendsAndSavedSearchesList()
   },
-  tapTxt: function (e) {},
+  tapTxt: function (e) { },
   tapAvatar: function (e) {
     fm.showUser(e.currentTarget.dataset.user)
   },
@@ -48,12 +49,47 @@ Page({
         that.setData({
           value: null
         })
+        for (const value of that.data.saved_searches) {
+          if (value.query === e.detail.value) {
+            return
+          }
+        }
+        ff.postPromise('/saved_searches/create', { query: e.detail.value })
+          .then(res => {
+            that.setData({
+              ['saved_searches[' + that.data.saved_searches.length + ']']: res.res
+            })
+          })
+          .catch(err => console.error(err))
       }
     })
   },
   tapListItem: function (e) {
     wx.navigateTo({
       url: `../feeds/feeds?q=${e.currentTarget.dataset.query}`
+    })
+  },
+  longpressListItem: function (e) {
+    const that = this
+    wx.showActionSheet({
+      itemList: ['删除'],
+      success: function (res) {
+        if (!res.cancel) {
+          ff.postPromise('/saved_searches/destroy', { id: e.currentTarget.dataset.id })
+            .then(res => {
+              // todo 删除前端的话题
+              for (const [index, value] of that.data.saved_searches.entries()) {
+                if (value.id === e.currentTarget.dataset.id) {
+                  that.setData({
+                    ['saved_searches[' + index + ']']: {} // 作用是删除这一条，但写 null 会报错，写 {} 生效 @饭小默
+                  })
+                  return
+                }
+              }
+            })
+            .catch(err => console.error(err))
+        }
+      }
     })
   }
 })
