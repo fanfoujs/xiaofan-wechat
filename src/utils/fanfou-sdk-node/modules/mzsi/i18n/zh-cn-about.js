2819066b940c1da1cb6843c57451006330ba29c9
LitoMore <litomore@gmail.com>
2017-10-02 19:57:38 +0800

    revise zodiac sign name

--- src/utils/fanfou-sdk-node/modules/mzsi/i18n/zh-cn-about.js
+++ src/utils/fanfou-sdk-node/modules/mzsi/i18n/zh-cn-about.js
@@ -1,50 +1,50 @@
 module.exports = {
   '1': {
-    'name': '水瓶',
+    'name': '水瓶座',
     'element': '风'
   },
   '2': {
-    'name': '双鱼',
+    'name': '双鱼座',
     'element': '水'
   },
   '3': {
-    'name': '白羊',
+    'name': '白羊座',
     'element': '火'
   },
   '4': {
-    'name': '金牛',
+    'name': '金牛座',
     'element': '土'
   },
   '5': {
-    'name': '双子',
+    'name': '双子座',
     'element': '风'
   },
   '6': {
-    'name': '巨蟹',
+    'name': '巨蟹座',
     'element': '水'
   },
   '7': {
-    'name': '狮子',
+    'name': '狮子座',
     'element': '火'
   },
   '8': {
-    'name': '处女',
+    'name': '处女座',
     'element': '土'
   },
   '9': {
-    'name': '天秤',
+    'name': '天秤座',
     'element': '风'
   },
   '10': {
-    'name': '天蝎',
+    'name': '天蝎座',
     'element': '水'
   },
   '11': {
-    'name': '射手',
+    'name': '射手座',
     'element': '火'
   },
   '12': {
-    'name': '摩羯',
+    'name': '摩羯座',
     'element': '土'
   }
 }
