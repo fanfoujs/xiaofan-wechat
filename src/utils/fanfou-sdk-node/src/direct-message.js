d308ddc7ac82af0cae2cc1af52f4bc10b75cde97
LitoMore <litomore@gmail.com>
2017-10-03 18:38:26 +0800

    fix created_at key

--- src/utils/fanfou-sdk-node/src/direct-message.js
+++ src/utils/fanfou-sdk-node/src/direct-message.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const timeago = require('../../timeago')
+const dateFormat = require('../modules/date-format/index')
 const User = require('./user')
 
 class DirectMessage {
@@ -8,7 +10,7 @@ class DirectMessage {
     this.text = dm.text
     this.sender_id = dm.sender_id
     this.recipient_id = dm.recipient_id
-    this.create_at = dm.create_at
+    this.created_at = dm.created_at
     this.sender_screen_name = dm.sender_screen_name
     this.recipient_screen_name = dm.recipient_screen_name
     this.sender = new User(dm.sender)
@@ -16,6 +18,29 @@ class DirectMessage {
     if (dm.in_reply_to) {
       this.in_reply_to = new DirectMessage(dm.in_reply_to)
     }
+
+    this.time_ago = this._getTimeAgo()
+    this.time_tag = this._getTimeTag()
+  }
+
+  _getTimeAgo () {
+    return timeago().format(this.created_at, 'fanfou_weapp')
+  }
+
+  _getTimeTag () {
+    const date = new Date()
+    const create = new Date(this.created_at)
+    const nowYear = dateFormat('yyyy', date)
+    const createYear = dateFormat('yyyy', create)
+    const nowDate = dateFormat('yyyyMMdd', date)
+    const createDate = dateFormat('yyyyMMdd', create)
+
+    if (nowDate === createDate) {
+      return dateFormat('hh:mm', create)
+    } else if (nowYear === createYear) {
+      return dateFormat('MM/dd hh:mm', create)
+    }
+    return dateFormat('yyyy/MM/dd hh:mm', create)
   }
 }
 
