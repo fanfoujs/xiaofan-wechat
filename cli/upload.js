3109cdc8a3e24866bd8de463443959b5ec68bc46
LitoMore <litomore@gmail.com>
2017-12-17 00:16:23 +0800

    Add CLI tool for upload

---
+++ cli/upload.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+'use strict'
+
+const path = require('path')
+const ora = require('ora')
+const execa = require('execa')
+
+const pkg = require('../package')
+
+const macCLI = '/Applications/wechatwebdevtools.app/Contents/Resources/app.nw/bin/cli'
+const {version} = pkg
+const xiaofanPath = path.join(__dirname, '../sc')
+
+process.spinner = ora('Uploading...').start()
+
+const run = async () => {
+  try {
+    const {stdout} = await execa(macCLI, ['-u', `${version}@${xiaofanPath}`], {timeout: 10000})
+    if (stdout.match('upload success')) {
+      process.spinner.succeed('上传成功！')
+      process.exit()
+    }
+    process.spinner.fail('上传超时！')
+    process.exit()
+  } catch (err) {
+    const match = err.message.match(/error: '{"code":\d+,"error":"(.+)"}',/)
+    const message = match[1]
+    process.spinner.fail(message + '！')
+    process.exit()
+  }
+}
+
+run()
