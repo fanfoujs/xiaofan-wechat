0b20ca2484a1bb9c06e88b0cff578b522c508c3e
mogita <me@mogita.com>
2018-01-07 02:56:26 +0800

    Add spanish language

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,6 +2,7 @@
 
 const enUS = require('./languages/en-us')
 const zhCN = require('./languages/zh-cn')
+const esES = require('./languages/es-es')
 
 const {language: langCode} = wx.getSystemInfoSync()
 const DEFAULT_LANG = enUS
@@ -13,6 +14,9 @@ switch (langCode) {
     break
   case 'zh_CN':
     lang = zhCN
+    break
+  case 'es':
+    lang = esES
     break
   default:
     break
---
+++ src/i18n/languages/es-es.js
@@ -0,0 +1,185 @@
+'use strict'
+
+module.exports = {
+  home: {
+    title: 'Xiaofan'
+  },
+  feed: {
+    title: 'Xiaofan',
+    copy: 'Copiar',
+    delete_confrim: '¿Eliminar este post?',
+    deleted: 'Eliminado',
+    repost: 'Reenviar',
+    reposted: 'Reenviado',
+    replied: 'Respondido',
+    from: 'Desde',
+    reply_to: 'Responder a',
+    published: 'Publicado'
+  },
+  login: {
+    title: 'Xiaofan',
+    username: 'Nombre de usuario',
+    password: 'Contraseña',
+    submit: 'Iniciar Sesión',
+    fetching: 'Cargando',
+    fail: 'Error de inicio de sesión',
+    logout: 'Cerrar Sesión'
+  },
+  mentions: {
+    title: 'Menciones',
+    all: 'Todo',
+    reply: 'Respuestas'
+  },
+  compose: {
+    title: 'Redactar',
+    placeholder: '¿Qué está pasando?',
+    reset: 'Reiniciar',
+    close: 'Cerrar',
+    attachment: 'Añadir una Foto',
+    send: 'Enviar',
+    remove_attachment: 'Eliminar la Foto',
+    sent: 'Enviado'
+  },
+  discover: {
+    title: 'Descubrir',
+    search_placeholder: 'Buscar en Fanfou',
+    hot_trends: 'Tendencias',
+    saved_trends: 'Búsquedas Guardadas',
+    public_timeline: 'Cronología Pública'
+  },
+  me: {
+    title: 'Yo',
+    location: 'Ubicación: ',
+    website: 'Sitio Web: ',
+    bio: 'Biografía: ',
+    birth: 'Cumpleaños: ',
+    fanfou_age: 'Se unió desde hace ',
+    statuses: 'Posts',
+    photos: 'Fotos',
+    likes: 'Favoritos',
+    following: 'Siguiendo',
+    followers: 'Seguidores',
+    friend_requests: 'Solicitud de Amistad',
+    direct_messages: 'Mensaje Directo',
+    profiles: 'Perfil',
+    switch_account: 'Cerrar Sesión',
+    about_xiaofan: 'Xiaofan',
+    followed_each_other: 'Nos seguimos mutuamente',
+    become_friend: 'Seguir',
+    remove_friend: 'Dejar de Seguir',
+    followed: 'Siguiendo',
+    block: 'Bloquear',
+    blocked: 'Bloqueado',
+    unblock: 'Desbloquear',
+    follow: 'Seguir',
+    unfollow: 'Dejar de Seguir',
+    avatar_updated: 'Tu foto de perfil ha cambiado',
+    profile_updated: 'Tu perfil ha cambiado',
+    register_today: 'Menos de un día',
+    year: '-',
+    month: '-',
+    day: ''
+  },
+  change_profile: {
+    nickname: 'Nombre',
+    location: 'Ubicación',
+    website: 'Sitio Web',
+    bio: 'Biografía'
+  },
+  request: {
+    name: 'Solicitud',
+    accept: 'Aceptar',
+    accept_and_follow: 'Aceptar y Seguir',
+    accepted: 'Aceptado',
+    reject: 'Rechazar'
+  },
+  common: {
+    save: 'Guardar',
+    error: 'Error',
+    copied: 'Copiado',
+    confirm: 'OK',
+    delete: 'Eliminar',
+    ok: 'OK',
+    she: 'Ella',
+    her: 'Su',
+    hers: 'Su',
+    he: 'Él',
+    him: 'Él',
+    his: 'Su',
+    she_he: 'Ella/Él',
+    her_him: 'Su',
+    hers_his: 'Su',
+    no_more: 'No más resultado',
+    just: 'Solo ',
+    years: ' Años ',
+    months: ' Meses ',
+    days: ' Días',
+    change_bg_info: 'Enlace copiado'
+  },
+  timeago: [
+    ['1s', 'hace poco'],
+    ['%ss', '%ss'],
+    ['1m', '1m'],
+    ['%sm', '%sm'],
+    ['1h', '1h'],
+    ['%sh', '%sh'],
+    ['1 día', '1 día'],
+    ['%s días', '%s días'],
+    ['1 semana', '1 semana'],
+    ['%s semanas', '%s semanas'],
+    ['1 mes', '1 mes'],
+    ['%s meses', '%s meses'],
+    ['1 año', '1 año'],
+    ['%s años', '%s años']
+  ],
+  zodiac_sign: {
+    1: {
+      name: 'Acuario',
+      element: 'El aire'
+    },
+    2: {
+      name: 'Piscis',
+      element: 'El agua'
+    },
+    3: {
+      name: 'Aries',
+      element: 'El fuego'
+    },
+    4: {
+      name: 'Tauro',
+      element: 'La tierra'
+    },
+    5: {
+      name: 'Géminis',
+      element: 'El aire'
+    },
+    6: {
+      name: 'Cáncer',
+      element: 'El agua'
+    },
+    7: {
+      name: 'Leo',
+      element: 'El fuego'
+    },
+    8: {
+      name: 'Virgo',
+      element: 'La tierra'
+    },
+    9: {
+      name: 'Libra',
+      element: 'El aire'
+    },
+    10: {
+      name: 'Escorpio',
+      element: 'El agua'
+    },
+    11: {
+      name: 'Sagitario',
+      element: 'El fuego'
+    },
+    12: {
+      name: 'Capricornio',
+      element: 'La tierra'
+    }
+  }
+}
