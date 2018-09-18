3823ea361d9cf102c0a47c5c03f1a432355e9430
LitoMore <litomore@gmail.com>
2018-09-18 15:23:43 +0800

    Add Chinese Traditional translation

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,6 +3,8 @@
 const enUS = require('./languages/en-us')
 const zhCN = require('./languages/zh-cn')
 const esES = require('./languages/es-es')
+const zhHK = require('./languages/zh-hk')
+const zhTW = require('./languages/zh-tw')
 
 const {language: langCode} = wx.getSystemInfoSync()
 const DEFAULT_LANG = enUS
@@ -14,6 +16,12 @@ switch (langCode) {
     break
   case 'zh_CN':
     lang = zhCN
+    break
+  case 'zh_HK':
+    lang = zhHK
+    break
+  case 'zh_TW':
+    lang = zhTW
     break
   case 'es':
     lang = esES
---
+++ src/i18n/languages/zh-tw.js
@@ -0,0 +1,202 @@
+'use strict'
+
+module.exports = {
+  lang: 'zhTW',
+  home: {
+    title: '小飯'
+  },
+  feed: {
+    title: '小飯',
+    copy: '複製',
+    delete_confrim: '確認刪除？',
+    deleted: '已刪除',
+    repost: '轉推',
+    reposted: '已轉推',
+    replied: '已回復',
+    from: '來自',
+    reply_to: '回復至',
+    published: '已發表'
+  },
+  login: {
+    title: '小飯',
+    username: '帳號',
+    password: '密碼',
+    submit: '登錄',
+    fetching: '登錄中',
+    fail: '登錄失敗',
+    logout: '登出'
+  },
+  mentions: {
+    title: '提及我的',
+    all: '所有',
+    reply: '提及'
+  },
+  compose: {
+    title: '新狀態',
+    placeholder: '你在做什麼？',
+    reset: '清除',
+    close: '放棄',
+    attachment: '上傳照片',
+    send: '發表',
+    remove_attachment: '刪除',
+    sent: '已發表'
+  },
+  discover: {
+    title: '發現',
+    search_placeholder: '搜索消息',
+    hot_trends: '熱門話題',
+    saved_trends: '關注的話題',
+    public_timeline: '隨便看看'
+  },
+  me: {
+    title: '我',
+    location: '所在地：',
+    website: '網址：',
+    bio: '自述：',
+    birth: '生日：',
+    fanfou_age: '飯齡：',
+    statuses: '狀態',
+    photos: '相片',
+    likes: '收藏',
+    timeline: '看到的',
+    following: '關注',
+    followers: '被關注',
+    friend_requests: '好友請求',
+    direct_messages: '私信',
+    profiles: '修改資料',
+    settings: '設置',
+    switch_account: '切換帳號',
+    about_xiaofan: '小飯',
+    followed_each_other: '好友',
+    become_friend: '加好友',
+    remove_friend: '取消關注',
+    followed: '已關注',
+    block: '拉黑',
+    blocked: '已拉黑',
+    unblock: '解除拉黑',
+    follow: '關注',
+    unfollow: '取消關注',
+    avatar_updated: '已更新頭像',
+    profile_updated: '已更新資料',
+    register_today: 'Less then 1 day',
+    year: ' 年 ',
+    month: ' 月 ',
+    day: '日',
+    change_bg_info: '背景圖只能網頁修改，鏈接已複製。',
+    not_friend_yet: '對方還沒關注你，請使用網頁版發送私信。'
+  },
+  change_profile: {
+    nickname: '暱稱',
+    location: '所在地',
+    website: '網址',
+    bio: '自述'
+  },
+  request: {
+    name: '關注申請',
+    accept: '接受',
+    accept_and_follow: '接受並關注',
+    accepted: '已接受',
+    reject: '拒絕'
+  },
+  common: {
+    save: '保存',
+    error: '錯誤',
+    copied: '鏈接已複製，請前往瀏覽器訪問。',
+    confirm: '好的',
+    delete: '刪除',
+    ok: '好的',
+    cancel: '取消',
+    she: '她',
+    her: '她的',
+    hers: '她的',
+    he: '他',
+    him: '他',
+    his: '他的',
+    she_he: 'TA',
+    her_him: 'TA',
+    her_his: 'TA 的',
+    hers_his: 'TA 的',
+    no_more: '沒有更多了',
+    just: '正好 ',
+    years: ' 年 ',
+    months: ' 個月 ',
+    days: ' 天'
+  },
+  dm: {
+    me: '我：'
+  },
+  settings: {
+    status_display: '顯示設置',
+    time: '時間',
+    source_name: '小尾巴',
+    status_count: '每頁消息數量',
+    blocks: '屏蔽選項',
+    hide_blocked_users: '屏蔽拉黑用戶'
+  },
+  timeago: [
+    ['1 秒前', '剛剛'],
+    ['%s 秒前', '%s 秒內'],
+    ['1 分鐘前', '1 分鐘內'],
+    ['%s 分鐘前', '%s 分鐘內'],
+    ['1 小時前', '1 小時內'],
+    ['%s 小時前', '%s 小時內'],
+    ['1 天前', '1 天內'],
+    ['%s 天前', '%s 天內'],
+    ['1 週前', '1 周內'],
+    ['%s 週前', '%s 周內'],
+    ['1 月前', '1 个月內'],
+    ['%s 月前', '%s 个月內'],
+    ['1 年前', '1 年內'],
+    ['%s 年前', '%s 年內']
+  ],
+  zodiac_sign: {
+    1: {
+      name: '水瓶座',
+      element: '風'
+    },
+    2: {
+      name: '雙魚座',
+      element: '水'
+    },
+    3: {
+      name: '白羊座',
+      element: '火'
+    },
+    4: {
+      name: '金牛座',
+      element: '土'
+    },
+    5: {
+      name: '雙子座',
+      element: '風'
+    },
+    6: {
+      name: '巨蟹座',
+      element: '水'
+    },
+    7: {
+      name: '獅子座',
+      element: '火'
+    },
+    8: {
+      name: '處女座',
+      element: '土'
+    },
+    9: {
+      name: '天秤座',
+      element: '風'
+    },
+    10: {
+      name: '天蠍座',
+      element: '水'
+    },
+    11: {
+      name: '射手座',
+      element: '火'
+    },
+    12: {
+      name: '摩羯座',
+      element: '土'
+    }
+  }
+}
