3b8d4e81d42e4c0de53e3b318a74b2ae00739722
LitoMore <litomore@gmail.com>
2021-10-02 12:31:09 +0800

    Drop outdated content & fix status bar height

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,4 +1,15 @@
 const extend = require('../../utils/extend')
 const tap = require('../../mixins/tap')
 
-Page(extend({}, tap, {}))
+Page(
+  extend(
+    {},
+    tap,
+    {},
+    {
+      data: {
+        statusBarHeight: wx.getSystemInfoSync().statusBarHeight,
+      },
+    },
+  ),
+)
