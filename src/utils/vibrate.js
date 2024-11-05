dde3f368d2955372a6a2b94e0e8cff9be19e3c1a
LitoMore <litomore@gmail.com>
2024-11-05 20:05:17 +0800

    Bump all dependencies to latest & cleanups

--- src/utils/vibrate.js
+++ src/utils/vibrate.js
@@ -1,5 +1,6 @@
 const {getSettings} = require('./util')
 
+// eslint-disable-next-line unicorn/no-anonymous-default-export
 module.exports = () => {
   if (getSettings().vibrate) {
     wx.vibrateShort()
